ee98ea003ae172687c370c54d08c65633e530bf4
Aks <tayalaks2001@gmail.com>
2022-06-16 16:58:44 +0530

    Linear time solution attempt on max circular sum  problem- unsuccessful

--- 15Jun.cpp
+++ 15Jun.cpp
@@ -19,7 +19,36 @@ int maxCircularSum(int n, vector<int> a){
 			maxSum = max(maxSum, s);
 		}
 	}
-
+	
 	return maxSum;
+}
+
+// attempt at O(n) solution - doesn't work
+// Problem is because we limit the size to n while traversing, any unnecessary prefix limits till where we can go later
+// Example -> 1 2 3 -4 1
+// the prefix 1 2 3 -4 has sum positive, which is why i don't start new sum
+// But if I did, I could get the array -> 1 1 2 3
+// basically in this when size exceeds n, I just restart new count from current pos. This needs to change to start from the pos of the last negative number
+int maxCircularSum2(int n, vector<int> a){
+    for (int i=0; i<n-1; i++)
+        a.push_back(a[i]);
+    int cs = 0, ms = INT_MIN, currStart = 0;
+    
+    for (int i=0; i<2*n-1; i++){
+        if (currStart >= n)
+            break;
+        
+        if (i-currStart >= n){
+            cs = 0;
+            currStart = i;
+        }
+        cs += a[i];
+        ms = max(ms, cs);
+        if (cs <= 0){
+            cs = 0;
+            currStart = i+1;
+        }
+    }
+	return ms;
 }
 
