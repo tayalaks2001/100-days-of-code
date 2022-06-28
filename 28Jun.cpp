f85231fe8969bb1a20e1739580350a26bf6cc9fe
Aks <tayalaks2001@gmail.com>
2022-06-28 22:55:05 +0530

    Question link added

--- 28Jun.cpp
+++ 28Jun.cpp
@@ -1,43 +1,37 @@
-// Question - 
+// Question - https://leetcode.com/problems/magnetic-force-between-two-balls/ 
 
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isFeasible(int ar[], int n, int k, int gap){
-	int pos = ar[0], filled = 1;
-	for (int i=1; i<n; i++){
-		if (ar[i]-pos >= gap){
-			filled++;
-			pos = ar[i];
-		}
+bool isFeasible(vector<int>& position, int n, int k, int gap){
+        int pos = position[0], filled = 1;
+        for (int i=1; i<n; i++){
+            if (position[i]-pos >= gap){
+                filled++;
+                pos = position[i];
+            }
 
-		if (filled == k)
-			return true;
-	}
+            if (filled == k)
+                return true;
+        }
 
-	return false;
-}
+        return false;
+    }
 
-int main() {
-	int n,k;
-	cin>>n>>k;
-	int ar[n];
-	for (int i=0; i<n; i++)
-		cin>>ar[i];
-	sort(ar, ar+n);
-	
-	int s = 1, e = (ar[n-1]-ar[0])/(k-1);
-	int ans = 0;
-	while (s <= e){
-		int m = (s+e)/2;
-		if (isFeasible(ar,n,k,m)){
-			ans = m;
-			s = m+1;
-		}
-		else
-			e = m-1;
-	}
-	
-	cout<<ans;
-	return 0;
-}
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(), position.end());
+        int n = position.size();
+        int s = 1, e = (position[n-1]-position[0])/(m-1);
+        int ans = 0;
+        while (s <= e){
+            int mid = (s+e)/2;
+            if (isFeasible(position,n,m,mid)){
+                ans = mid;
+                s = mid+1;
+            }
+            else
+                e = mid-1;
+        }
+        
+        return ans;
+    }
