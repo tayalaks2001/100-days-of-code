4708a2d48ecf66b52cf8a43057f36f26ce72fa41
Aks <tayalaks2001@gmail.com>
2022-08-05 08:39:05 +0400

    Code for 30 Jul

---
+++ 30Jul.cpp
@@ -0,0 +1,63 @@
+// Question - alternative method to print tree level-wise; more mem efficient than before
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+    int data;
+    node *l,*r;
+    node(int d){
+        data = d;
+        l = r = NULL;
+    }
+};
+
+node* buildTree(){
+    int d;
+    cin>>d;
+    if (d == -1)
+        return NULL;
+    
+    node* root = new node(d);
+    root->l = buildTree();
+    root->r = buildTree();
+
+    return root;
+}
+
+void replaceWithSum(node* &root){
+    if (root == NULL)
+        return;
+    
+    replaceWithSum(root->l);
+    replaceWithSum(root->r);
+    if (root->l != NULL)
+        root->data += root->l->data;
+    if (root->r != NULL)
+        root->data += root->r->data;
+}
+
+void printLevelwise(node* root){
+    queue<node*> traversal;
+    traversal.push(root);
+    traversal.push(NULL);
+    while(!traversal.empty()){
+        node* curr = traversal.front();
+        if (curr == NULL){
+            cout<<endl;
+            traversal.pop();
+            if (!traversal.empty()){
+                traversal.push(NULL);
+            }
+            continue;
+        }
+        cout<<curr->data<<" ";
+        traversal.pop();
+        if (curr->l != NULL)
+            traversal.push(curr->l);
+        if (curr->r != NULL)
+            traversal.push(curr->r);
+    }
+    cout<<endl;
+}
